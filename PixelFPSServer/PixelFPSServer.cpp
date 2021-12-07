410cb03ad88bff7ff73b925a4b9f002b40d3816f
GreatDestroyerCharlie <1669247240@qq.com>
2021-12-07 20:48:55 +0300

    add asio, update simple demo

--- PixelFPSServer/PixelFPSServer.cpp
+++ PixelFPSServer/PixelFPSServer.cpp
@@ -1,10 +1,57 @@
 ﻿#include "PixelFPSServer.h"
 
 using namespace std;
+using namespace asio;
+using namespace asio::ip;
 
 int main()
 {
-	cout << "Hello CMake.\n";
-	cin.get();
-	return 0;
+    cout << "Hello CMake.\n";
+
+    error_code ec;
+
+    io_context context;
+
+    tcp::endpoint endPoint(make_address("93.184.216.34", ec), 80);
+
+    tcp::socket socket(context);
+
+    socket.connect(endPoint, ec);
+
+    if (!ec)
+    {
+        cout << "Connected!\n";
+    }
+    else
+    {
+        cout << "Failed!\n" + ec.message() + "\n";
+    }
+
+    if (socket.is_open())
+    {
+        string request = "GET /index.html HTTP/1.1\r\n"
+            "Host: example.com\r\n"
+            "Connection: close\r\n\r\n";
+
+        socket.write_some(buffer(request.data(), request.size()), ec);
+
+        this_thread::sleep_for(2000ms);
+
+        size_t bytes = socket.available();
+        cout << "Bytes Available:" << bytes << "\n";
+
+        if (bytes > 0)
+        {
+            vector<char> data_buffer(bytes);
+            socket.read_some(buffer(data_buffer.data(), data_buffer.size()), ec);
+
+            for (auto& c : data_buffer)
+            {
+                cout << c;
+            }
+        }
+    }
+
+    cin.get();
+    return 0;
 }
--- PixelFPSServer/PixelFPSServer.h
+++ PixelFPSServer/PixelFPSServer.h
@@ -1,3 +1,21 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+//asio lib
+#include "asio.hpp"
+#include "asio/ts/buffer.hpp"
+#include "asio/ts/internet.hpp"
+
+//platform API
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef __linux__
+#include <unistd.h>
+#endif
